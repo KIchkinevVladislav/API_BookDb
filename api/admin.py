e3ee96f9f28d86ae6ad3ff69798eb31191da7898
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-04-02 21:41:18 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Genre, Book, Review, Comment, User
 
+#регистрируем модели для админ-панели
 admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(Review)
