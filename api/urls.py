9f7d644e9fd4d85c9d622c511c657384bc72f7a8
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-05-18 23:38:49 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
 from rest_framework_simplejwt.views import TokenRefreshView
-
 from .views import (
     RegisterView,
     TokenView,
